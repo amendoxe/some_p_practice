db276f139e9e361f04c08036b39a0da5c5174815
amendoxe <arturo_mendoza@outlook.com>
2024-07-02 16:16:40 -0600

    Add other images

--- say.py
+++ say.py
@@ -1,4 +1,13 @@
 """We are going to do some library testing"""
 import cowsay
+import sys
 
-cowsay.cow("Well hellooo")
+if len(sys.argv) > 1:
+    message = ""
+    for arg in sys.argv[1:]:
+        message += arg
+        message += " "
+    cowsay.stegosaurus(message)
+    cowsay.octopus(message)
+    cowsay.pig(message)
+    cowsay.tux(message)
